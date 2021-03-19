61867beb1eeca661e42e4b63ea82518de9eb83f3
woahdas <78304648+woahdas@users.noreply.github.com>
2021-03-19 17:57:13 +0000

    Update BasicSkeleton.r

--- BasicSkeleton.r
+++ BasicSkeleton.r
@@ -1,7 +1,12 @@
 #load and plot the MNIST database
-library(readr)library(dplyr)mnist_raw <- read_csv("https://pjreddie.com/media/files/mnist_train.csv", col_names = FALSE)
-library(tidyr)pixels_gathered <- mnist_raw %>%  head(10000) %>%  rename(label = X1) %>%  mutate(instance = row_number()) %>%  gather(pixel, value, -label, -instance) %>%  tidyr::extract(pixel, "pixel", "(\\d+)", convert = TRUE) %>%  mutate(pixel = pixel - 2,         x = pixel %% 28,         y = 28 - pixel %/% 28)pixels_gathered
-library(ggplot2)theme_set(theme_light())pixels_gathered %>%  filter(instance <= 12) %>%  ggplot(aes(x, y, fill = value)) +  geom_tile() +  facet_wrap(~ instance + label)
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+mnist_raw <- read_csv("https://pjreddie.com/media/files/mnist_train.csv", col_names = FALSE)
+pixels_gathered <- mnist_raw %>%  head(10000) %>%  rename(label = X1) %>%  mutate(instance = row_number()) %>%  gather(pixel, value, -label, -instance) %>%  tidyr::extract(pixel, "pixel", "(\\d+)", convert = TRUE) %>%  mutate(pixel = pixel - 2,         x = pixel %% 28,         y = 28 - pixel %/% 28)pixels_gathered
+theme_set(theme_light())pixels_gathered %>%  filter(instance <= 12) %>%  ggplot(aes(x, y, fill = value)) +  geom_tile() +  facet_wrap(~ instance + label)
 
 
 #create centroids of the numbers 0-9, which is just creating the 'average' 6 or 'average' 7 from this database
@@ -10,14 +15,14 @@ averagedigits <- library(ggplot2)pixel_summary %>%  ggplot(aes(x, y, fill = mean
 
 #play notes
 
-library(tuneR)        #import the tuneR library
-f=392                 #frequency of G4 note
+library(tuneR)
+f=392
 sr=8000 
 bits=16
-secs=2                #length of the note set to 2
+secs=2           
 amp=1
 t=seq(0, secs, 1/sr)
-y= amp*sin(2*pi*f*t)  #make a sinewave with above attributes
-s=floor(2^(bits-2)*y) #floor it to make it an integer value
-u=Wave(s, samp.rate=sr, bit=bits)  #make a wave structure 
+y= amp*sin(2*pi*f*t)  
+s=floor(2^(bits-2)*y) 
+u=Wave(s, samp.rate=sr, bit=bits)
 play(u)
